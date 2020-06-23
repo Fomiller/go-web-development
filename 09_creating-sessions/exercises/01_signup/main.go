ecb7bb69a8bca5ae88e13997da0db49a8946d8b5
Forrest <Forrestmillerj@gmail.com>
2020-06-23 16:21:53 -0500

    completed creating sessions lesson

---
+++ 09_creating-sessions/exercises/01_signup/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"net/http"
+	"text/template"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+type user struct {
+	UserName string
+	Password string
+	First    string
+	Last     string
+}
+
+var tpl *template.Template
+var dbUsers = map[string]user{}
+var dbSessions = map[string]string{}
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/bar", bar)
+	http.HandleFunc("/signup", signup)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(res http.ResponseWriter, req *http.Request) {
+	u := getUser(req)
+	tpl.ExecuteTemplate(res, "index.gohtml", u)
+}
+
+func bar(res http.ResponseWriter, req *http.Request) {
+	u := getUser(req)
+	if !alreadyLoggedIn(req) {
+		http.Redirect(res, req, "/", http.StatusSeeOther)
+		return
+	}
+	tpl.ExecuteTemplate(res, "bar.gohtml", u)
+}
+
+func signup(res http.ResponseWriter, req *http.Request) {
+	if alreadyLoggedIn(req) {
+		http.Redirect(res, req, "/", http.StatusSeeOther)
+	}
+
+	// process form submission
+	if req.Method == http.MethodPost {
+
+		// get form values
+		un := req.FormValue("username")
+		p := req.FormValue("password")
+		f := req.FormValue("firstname")
+		l := req.FormValue("lastname")
+
+		// check if username is taken
+		if _, ok := dbUsers[un]; ok {
+			http.Error(res, "Username is already Taken", http.StatusForbidden)
+			return
+		}
+
+		// create session
+		sID, _ := uuid.NewV4()
+		c := &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+		http.SetCookie(res, c)
+		dbSessions[c.Value] = un
+
+		// store user in dbUsers
+		u := user{un, p, f, l}
+		dbUsers[un] = u
+
+		// redirect
+		http.Redirect(res, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	tpl.ExecuteTemplate(res, "signup.gohtml", nil)
+}
